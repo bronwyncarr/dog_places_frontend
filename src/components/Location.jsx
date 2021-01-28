18f16348fbde5a0b05a647ac686422a6171870c5
bronwyncarr <bronwynacarr@gmail.com>
2021-01-28 12:25:43 +1100

    Adds fetch request on localhost

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -1,7 +1,27 @@
-function Location() {
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+function Location(props) {
+  const [location, setLocation] = useState([]);
+  const id = props.match.params.id;
+  useEffect(() => {
+    fetch(`http://localhost:3000/api/locations/${id}`)
+      .then((res) => res.json())
+      .then((data) => setLocation(data));
+  }, [id]);
+
   return (
     <>
-      <h1>Location</h1>
+      <h1>{location.name}</h1>
+      <Link
+        to="/"
+        onclick={(e) => {
+          props.history.goBack();
+        }}
+      >
+        {" "}
+        Back
+      </Link>
     </>
   );
 }
