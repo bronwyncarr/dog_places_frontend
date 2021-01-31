8c582eebb12eaf86a4200233a76691982cec0bbf
bronwyncarr <bronwynacarr@gmail.com>
2021-02-01 10:17:36 +1100

    Update with reducer and username/pw combo

--- src/components/NewSession.jsx
+++ src/components/NewSession.jsx
@@ -1,17 +1,26 @@
 import React, { useState } from "react";
+import { signIn } from "../services/authServices";
+import { useGlobalState } from "../utils/context";
 
 function NewSession({ history }) {
-  const [email, setEmail] = useState("");
-
-  const [password, setPassword] = useState("");
+  // single state object that contains user info
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
   const [errMessage, setErrMessage] = useState("");
+  const { dispatch } = useGlobalState();
 
   async function onFormSubmit(event) {
     event.preventDefault();
     const body = {
-      user: { email, password },
+      user: {
+        username: user.username,
+        password: user.password,
+      },
     };
     try {
+      // Some of this should be refactored into authServices
       const response = await fetch(`http://localhost:3000/api/auth/sign_in`, {
         method: "POST",
         headers: {
@@ -24,11 +33,16 @@ function NewSession({ history }) {
       } else {
         const { jwt } = await response.json();
         localStorage.setItem("token", jwt);
+        dispatch({ type: "setLoggedInUser", data: user.username });
         history.push("/");
       }
     } catch (err) {
       setErrMessage(err.message);
     }
+  }
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value });
   }
 
   return (
@@ -37,13 +51,13 @@ function NewSession({ history }) {
       {errMessage && <span>{errMessage}</span>}
       <form onSubmit={onFormSubmit}>
         <div className="form-group">
-          <label htmlFor="email">Email</label>
+          <label htmlFor="username">Email</label>
           <input
-            type="email"
-            name="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            type="test"
+            name="username"
+            id="username"
+            value={user.username}
+            onChange={handleChange}
           />
         </div>
 
@@ -53,8 +67,8 @@ function NewSession({ history }) {
             type="password"
             name="password"
             id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            value={user.password}
+            onChange={handleChange}
           />
         </div>
         <input id="submit" type="submit" value="Submit" />
