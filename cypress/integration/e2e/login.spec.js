1c2b25b0bfd6e9fe2f42612a69f19c50abea976a
Rodney Ditchfield <rodney.ditch@hotmail.com>
2021-02-06 17:10:14 +1100

    further cypres config

--- cypress/integration/e2e/login.spec.js
+++ cypress/integration/e2e/login.spec.js
@@ -1,15 +1,24 @@
 describe("when clicking on sign in from the homepage user", () => {
-  beforeEach(() => {
-    cy.visit("/");
-    cy.findByText(/sign in/).click();
+  before(() => {
+    cy.fixture("user.json").then((user)=>{
+      cy.visit("/sign_up");
+      cy.findByLabelText(/username/i).type(user.username);
+      cy.findByLabelText(/password/i).type(user.password);
+      cy.findByLabelText(/email/i).type(user.email);
+    })
+    
   });
 
   it("should go to the sign_in page", () => {
     cy.url().should("include", "/sign_in");
   });
-
-  it("should see username and password inputs", () => {
-    cy.findByLabelText(/username/i).should("exist");
-    cy.findByLabelText(/password/i).should("exist");
-  });
+  it(" should be able to click on the submit and be redirected to the location list",()=>{
+    cy.get('form').submit()
+    cy.url().should('eql',"http://localhost:8080/")
+  })
+  after(() => {
+    window.localStorage.removeItem("token")
+    window.sessionStorage.removeItem("auth")
+  })
+  
 });
---
+++ cypress/support/generate.js
@@ -0,0 +1,13 @@
+import { build, fake } from '@jackfranklin/test-data-bot'
+import { userBuilder } from "../support/generate";
+
+const userBuilder = build('User', {
+  fields: {
+    email: fake((f) => f.internet.email()),
+    password: fake((f) => f.internet.password())
+  }
+})
+
+export {
+  userBuilder
+}
