a5954298df24a0af65c1f42fc865b4654a10b683
bronwyncarr <bronwynacarr@gmail.com>
2021-02-07 13:59:08 +1100

    Adds alt text to favourites pg if no fav

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -1,9 +1,22 @@
 import useFavourites from "../../hooks/useFavourites";
 import LocationsContainer from "../Location/LocationsContainer";
+import { Link } from "react-router-dom";
 
 function Favourites() {
   const { favourites } = useFavourites();
-  return <LocationsContainer locations={favourites} />;
+
+  return favourites.length > 0 ? (
+    <LocationsContainer locations={favourites} />
+  ) : (
+    <>
+      <h2>You haven't selected any favourites yet.</h2>
+      <h4>
+        Why not have a look through some great locations on the
+        <Link to={`/`}>search page</Link> and click on the heart icon 🤍 to save
+        them for reference.
+      </h4>
+    </>
+  );
 }
 
 export default Favourites;
