a001e8135069e60e1ba32bcda19945c59e6ff4e8
bronwyncarr <bronwynacarr@gmail.com>
2021-02-08 23:06:58 +1100

    More trello boards in docs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,15 @@
 // This form is used to create a new and edit an existing location information.
 // A user must be signed in to create a new location, A user must be admin to edit a location
+import {
+  Wrapper,
+  StyledForm,
+  Field,
+  Input,
+  Label,
+  Heading,
+} from "../styles/tileStyles";
 
-import { Form, Checkboxes } from "../styles/NewLocation";
+import { Checkboxes } from "../styles/NewLocation";
 // Props for the form are passed in from the NewLcoation or Edit Location
 function GeneratedForm({
   details,
@@ -19,65 +27,70 @@ function GeneratedForm({
   }
 
   return (
-    <Form encType="multipart/form-data" onSubmit={handleSubmit}>
-      {/* Name, description and address fields */}
-      {fields.map((item, index) => {
-        return (
-          <div key={index}>
-            <label htmlFor={item}>{capitaliseName(item)}</label>
-            <input
-              type="text"
-              name={item}
-              id={item}
-              value={details[item]}
-              onChange={handleFormChange}
-            />
-          </div>
-        );
-      })}
+    <Wrapper>
+      <Heading>New Location</Heading>
+      <StyledForm encType="multipart/form-data" onSubmit={handleSubmit}>
+        {/* Name, description and address fields */}
+        {fields.map((item, index) => {
+          return (
+            <Field key={index}>
+              <Label htmlFor={item}>{capitaliseName(item)}:</Label>
+              <Input
+                type="text"
+                name={item}
+                id={item}
+                value={details[item]}
+                onChange={handleFormChange}
+              />
+            </Field>
+          );
+        })}
 
-      {/* Select option for category. Location types are retrived from static assets  */}
-      <label htmlFor="category">Category:</label>
-      <select
-        name="location_type_name"
-        id="location_type_name"
-        value={details.location_type_name}
-        onChange={handleFormChange}
-      >
-        <option value selected>
-          -- Please select --
-        </option>
-        {locationTypes &&
-          locationTypes.map((item, index) => (
-            <option key={index} value={item}>
-              {item}
+        {/* Select option for category. Location types are retrived from static assets  */}
+        <Field>
+          <Label htmlFor="category">Category:</Label>
+          <select
+            name="location_type_name"
+            id="location_type_name"
+            value={details.location_type_name}
+            onChange={handleFormChange}
+          >
+            <option value selected>
+              -- Please select --
             </option>
-          ))}
-      </select>
+            {locationTypes &&
+              locationTypes.map((item, index) => (
+                <option key={index} value={item}>
+                  {item}
+                </option>
+              ))}
+          </select>
+        </Field>
+        {/* Checkboxes for facilities. Facility types are retrived from static assets */}
+        <Checkboxes>
+          <p>Facilities</p>
+          {facilityTypes &&
+            facilityTypes.map((item, index) => {
+              return (
+                <div key={index}>
+                  <input
+                    type="checkbox"
+                    id={item}
+                    name={item}
+                    value={item}
+                    onChange={handleCheckChange}
+                  />
+                  <label htmlFor={item}> {capitaliseName(item)}</label>
+                </div>
+              );
+            })}
+        </Checkboxes>
 
-      {/* Checkboxes for facilities. Facility types are retrived from static assets */}
-      <Checkboxes>
-        {facilityTypes &&
-          facilityTypes.map((item, index) => {
-            return (
-              <div key={index}>
-                <input
-                  type="checkbox"
-                  id={item}
-                  name={item}
-                  value={item}
-                  onChange={handleCheckChange}
-                />
-                <label htmlFor={item}> {capitaliseName(item)}</label>
-              </div>
-            );
-          })}
-      </Checkboxes>
-
-      <button id="submit" type="submit" value="Submit">
-        Submit!
-      </button>
-    </Form>
+        <button id="submit" type="submit" value="Submit">
+          Submit!
+        </button>
+      </StyledForm>
+    </Wrapper>
   );
 }
 
