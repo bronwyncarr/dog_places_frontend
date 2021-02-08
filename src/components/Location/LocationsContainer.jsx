24dec2b6999b1f4cf0fb1838276be5c61a7c35f8
bronwyncarr <bronwynacarr@gmail.com>
2021-02-08 19:36:44 +1100

    Add styles to LocationsContainer

--- src/components/Location/LocationsContainer.jsx
+++ src/components/Location/LocationsContainer.jsx
@@ -1,32 +1,36 @@
 import LocationSummaryTile from "./LocationSummaryTile";
 import useFavourites from "../../hooks/useFavourites";
 import { Link } from "react-router-dom";
+import styled from "styled-components/macro";
+
+const Container = styled.div`
+  width: 100%;
+  overflow-y: auto;
+`;
 
 function LocationsContainer({ locations }) {
   const { favourites, addFavourite, removeFavourite } = useFavourites();
   const favouriteIds = favourites.map((fave) => fave.id);
 
   return (
-    <div>
+    <Container>
       {/* Once locations available, list all locations with show, edit, delete links. */}
       {locations &&
         locations.map((location) => {
           return (
             <LocationSummaryTile
-              // Heart icon to add/delete favourite.
               addFavourite={addFavourite}
               removeFavourite={removeFavourite}
               key={location.id}
               location={location}
-              // Includes returns a boolean if the value is in the array.
+              // includes returns a boolean if the value is in the array
               favourite={favouriteIds.includes(location.id)}
             >
-              {/* Passed in as children to the index and favourites page, not to the show. */}
               <Link to={`/locations/${location.id}`}>Show details</Link>
             </LocationSummaryTile>
           );
         })}
-    </div>
+    </Container>
   );
 }
 
