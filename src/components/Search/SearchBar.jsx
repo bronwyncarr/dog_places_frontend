97d6ab25b5874c3c5d55da68273d75af3a9402cb
bronwyncarr <bronwynacarr@gmail.com>
2021-02-08 23:04:06 +1100

    Initial searchbar styling

--- src/components/Search/SearchBar.jsx
+++ src/components/Search/SearchBar.jsx
@@ -1,18 +1,30 @@
+import { StyledForm, Field, Input } from "../../styles/formStyles";
+import styled from "styled-components/macro";
+
+export const SearchWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
 function SearchBar({ handleSearchSubmit, handleSearchChange, searchData }) {
   return (
-    <form onSubmit={handleSearchSubmit}>
-      <input
-        type="text"
-        id="searchBar"
-        name="searchBar"
-        value={searchData}
-        onChange={handleSearchChange}
-        placeholder="Search by name..."
-      ></input>
-      <button id="submit" type="submit" value="Submit">
-        Submit
-      </button>
-    </form>
+    <SearchWrapper>
+      <StyledForm onSubmit={handleSearchSubmit}>
+        <Field>
+          <Input
+            type="text"
+            id="searchBar"
+            name="searchBar"
+            value={searchData}
+            onChange={handleSearchChange}
+            placeholder="Search by name..."
+          />
+          <Input id="submit" type="submit" value="Submit" />
+        </Field>
+      </StyledForm>
+    </SearchWrapper>
   );
 }
 
